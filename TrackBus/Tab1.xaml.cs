4679a02b7312e598f49c4734245f627b8d851567
derbydxgit config --global user.email derbydx@hotmail.com <derbydx@hotmail.com>
2019-11-10 11:34:29 -0400

    list view & notas

--- TrackBus/Tab1.xaml.cs
+++ TrackBus/Tab1.xaml.cs
@@ -7,9 +7,45 @@ namespace TrackBus
 {
     public partial class Tab1 : ContentPage
     {
+        List<Autobus> Datos;
         public Tab1()
         {
+
             InitializeComponent();
+
+            Datos = new List<Autobus>();
+            Datos.Add(new Autobus()
+            {
+                Imagen = "autobus32.png",
+                Nombre = "Ruta 1 Cotui-Fantino",
+                Chofer = "Juan Perez"
+            });
+
+            Datos.Add(new Autobus()
+            {
+                Imagen = "autobus32.png",
+                Nombre = "Ruta 2 Cotui-Maimon",
+                Chofer = "Miguel Hernandez"
+            });
+
+            Datos.Add(new Autobus()
+            {
+                Imagen = "autobus32.png",
+                Nombre = "Ruta 3 Cotui-Cevicos",
+                Chofer = "Jefferson Smith"
+            });
+            ListaBus.ItemsSource = Datos;
         }
+
+    }
+    //1- Crear la clase autobus 
+    public class Autobus
+    {
+        public string Imagen { get; set; }
+        public string Nombre { get; set; }
+        public string Chofer { get; set; }
+
+
     }
 }
+
--- TrackBus/Tab2.xaml.cs
+++ TrackBus/Tab2.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 using Xamarin.Forms;
 
@@ -7,9 +8,30 @@ namespace TrackBus
 {
     public partial class Tab2 : ContentPage
     {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
         public Tab2()
         {
             InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
         }
     }
 }
